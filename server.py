d0a5d28565d1e8953f53afdc174654889e9a3017
tjamesbu <64707274+tjamesbu@users.noreply.github.com>
2021-08-19 21:03:27 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,24 @@
+from machinetranslation.translator import  englishToFrench, frenchToEnglish
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def english2French():
+    textToTranslate = request.args.get('textToTranslate')
+    french_text = englishToFrench(textToTranslate)
+    return french_text
+
+@app.route("/frenchToEnglish")
+def french2English():
+    textToTranslate = request.args.get('textToTranslate')
+    french_text = frenchToEnglish(textToTranslate)
+    return french_text
+
+@app.route("/")
+def renderIndexPage():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
